5d71daa73d1a77236b85180fb7cb6e30eb3e0d84
王英辉 <iyinghui@163.com>
2019-06-10 16:51:44 +0800

    A module And B module end

--- MainProject/ViewController.m
+++ MainProject/ViewController.m
@@ -7,17 +7,53 @@
 //
 
 #import "ViewController.h"
+#import <HandyFrame/UIView+LayoutMethods.h>
+#import "CTMediator+A.h"
+#import "CTMediator.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIButton *pushAViewControllerButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - life cycle
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.view addSubview:self.pushAViewControllerButton];
 }
 
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    [self.pushAViewControllerButton sizeToFit];
+    [self.pushAViewControllerButton centerEqualToView:self.view];
+}
+
+#pragma mark - event response
+- (void)didTappedPushAViewControllerButton:(UIButton *)button
+{
+//    AViewController *viewController = [[AViewController alloc] init];
+//    [self.navigationController pushViewController:viewController animated:YES];
+    
+    UIViewController *viewController = [[CTMediator sharedInstance] A_aViewController];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+#pragma mark - getters and setters
+- (UIButton *)pushAViewControllerButton
+{
+    if (_pushAViewControllerButton == nil) {
+        _pushAViewControllerButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_pushAViewControllerButton setTitle:@"push A view controller" forState:UIControlStateNormal];
+        [_pushAViewControllerButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_pushAViewControllerButton addTarget:self action:@selector(didTappedPushAViewControllerButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _pushAViewControllerButton;
+}
 
 @end
--- Podfile
+++ Podfile
@@ -1,11 +1,16 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
+source 'https://github.com/kymodular/PrivatePods.git'
+source 'https://github.com/CocoaPods/Specs.git'
 
 target 'MainProject' do
   # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
   # use_frameworks!
 
   # Pods for MainProject
+  pod "A_Category"
+  pod "A"
+  pod 'HandyFrame'
 
   target 'MainProjectTests' do
     inherit! :search_paths
